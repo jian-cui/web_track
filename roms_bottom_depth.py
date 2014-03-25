1530ad13db3cf158c1bbd6e7526a24451221d13b
jian.cui <acui145@gmail.com>
2014-03-25 15:56:34 -0400

    Runge Kutta Method

--- roms_bottom_depth.py
+++ roms_bottom_depth.py
@@ -3,8 +3,37 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
 import numpy as np
 import jata
-
-# url = 'http://tds.marine.rutgers.edu:8080/thredds/dodsC/roms/espresso/2013_da/avg_Best/ESPRESSO_Real-Time_v2_Averages_Best_Available_best.ncd?h[0:1:81][0:1:129],lon_rho[0:1:81][0:1:129],lat_rho[0:1:81][0:1:129]'
+import math
+def vertical_point(p1, p2, p0):
+    x1, y1 = p1[0], p1[1]
+    x2, y2 = p2[0], p2[1]
+    x3, y3 = p0[0], p0[1]
+    x = ((x2-x1)*(y2-y1)*(y3-y1)+(x2-x1)**2*x3+(y2-y1)**2*x1)/\
+        ((y2-y1)**2+(x2-x1)**2)
+    y = ((y2-y1)*x-x1*y2+x2*y1)/\
+        (x2-x1)
+    return x, y
+def value_on_proportion(p1, p2, p0, v1, v2):
+    '''
+    p1, p2, p0 are on the same line, v1, v2 are the value of p1, p2,
+    calculate and return the value of p0
+    '''
+    x1, y1 = p1[0], p1[1]
+    x2, y2 = p2[0], p2[1]
+    x0, y0 = p0[0], p0[1]
+    dist01 = math.sqrt((y0-y1)**2+(x0-x1)**2)
+    dist12 = math.sqrt((y1-y2)**2+(x1-x2)**2)
+    v3 = (v2-v1)*dist01/dist12+v1
+    return v3
+def left_button_down(event):
+    x, y = event.xdata, event.ydata
+    print x, y
+def rk_4(func, func(0, 0), h):
+    while n is not 0:
+        f[n] = rk_r()
+        f[n+1] = f[n]+h*(k1+2k2+2K3+k4)/6
+        k1 = func(x[n], y[n])
+        
 url = 'http://tds.marine.rutgers.edu:8080/thredds/dodsC/roms/espresso/2013_da/avg_Best/ESPRESSO_Real-Time_v2_Averages_Best_Available_best.ncd?h[0:1:81][0:1:129],temp[0:1:307][0:1:35][0:1:81][0:1:129],lon_rho[0:1:81][0:1:129],lat_rho[0:1:81][0:1:129]'
 data = jata.get_nc_data(url, 'lon_rho', 'lat_rho', 'h', 'temp')
 lonsize = np.amin(data['lon_rho'][:])-1, np.amax(data['lon_rho'][:])+1
@@ -13,6 +42,7 @@ latsize = np.amin(data['lat_rho'][:])-1, np.amax(data['lat_rho'][:])+1
 
 fig = plt.figure()
 ax = plt.subplot(111)
+fig.canvas.mpl_connect('button_press_event', left_button_down)
 dmap = Basemap(projection = 'cyl',
                llcrnrlat = min(latsize)-0.01,
                urcrnrlat = max(latsize)+0.01,
